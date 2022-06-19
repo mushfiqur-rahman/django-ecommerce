1953e4a89cfce3dd52c835fbeff4935dfc831d40
mushfiq-rahman <mushfiq.style@gmail.com>
2022-06-19 21:53:16 +0600

    product show

---
+++ store/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Category, Product
+
+
+# Create your views here.
+
+def categories(request):
+    return {
+        'categories': Category.objects.all()
+    }
+
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'store/home.html', {'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'store/products/detail.html', {'product': product})
